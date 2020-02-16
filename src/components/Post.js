3869774b39f6e8b98275b700fcdc435bfd59132a
Stanislaw Cruz <stanycruz@outlook.com>
2020-02-16 18:46:40 -0300

    Aula 4: Curtindo e descurtindo fotos no feed.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,93 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 
 const width = Dimensions.get('window').width;
 
 export default class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foto: this.props.foto, //{...this.props.foto, likers: [{}]},
+    };
+  }
+
+  carregaIcone(likeada) {
+    return likeada
+      ? require('../../resources/img/s2-checked.png')
+      : require('../../resources/img/s2.png');
+  }
+
+  exibeLikes(likers) {
+    if (likers.length <= 0) {
+      return;
+    }
+
+    return (
+      <Text style={styles.likes}>
+        {likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}
+      </Text>
+    );
+  }
+
+  exibeLegenda(foto) {
+    if (foto.comentario === '') {
+      return;
+    }
+
+    return (
+      <View style={styles.comentario}>
+        <Text style={styles.tituloComentario}>{foto.loginUsuario}</Text>
+        <Text>{foto.comentario}</Text>
+      </View>
+    );
+  }
+
+  like() {
+    const {foto} = this.state;
+    let novaLista = [];
+
+    if (!foto.likeada) {
+      novaLista = [...foto.likers, {login: 'meuUsuario'}];
+    } else {
+      novaLista = foto.likers.filter(liker => {
+        return liker.login !== 'meuUsuario';
+      });
+    }
+
+    const fotoAtualizada = {
+      ...foto,
+      likeada: !foto.likeada,
+      likers: novaLista,
+    };
+    this.setState({foto: fotoAtualizada});
+  }
+
   render() {
+    const {foto} = this.state;
     return (
       <View>
         <View style={styles.cabecalho}>
-          <Image
-            source={{uri: this.props.foto.urlPerfil}}
-            style={styles.fotoDePerfil}
-          />
-          <Text>{this.props.foto.loginUsuario}</Text>
+          <Image source={{uri: foto.urlPerfil}} style={styles.fotoDePerfil} />
+          <Text>{foto.loginUsuario}</Text>
         </View>
-        <Image source={{uri: this.props.foto.urlFoto}} style={styles.foto} />
+        <Image source={{uri: foto.urlFoto}} style={styles.foto} />
+        <View style={styles.rodape}>
+          <TouchableOpacity onPress={this.like.bind(this)}>
+            <Image
+              style={styles.botaoDeLike}
+              source={this.carregaIcone(foto.likeada)}
+            />
+          </TouchableOpacity>
+          {this.exibeLikes(foto.likers)}
+          {this.exibeLegenda(foto)}
+        </View>
       </View>
     );
   }
@@ -35,5 +108,23 @@ const styles = StyleSheet.create({
   foto: {
     width: width,
     height: width,
+  },
+  botaoDeLike: {
+    marginBottom: 10,
+    width: 40,
+    height: 40,
+  },
+  rodape: {
+    margin: 10,
+  },
+  likes: {
+    fontWeight: 'bold',
+  },
+  comentario: {
+    flexDirection: 'row',
+  },
+  tituloComentario: {
+    fontWeight: 'bold',
+    marginRight: 5,
   },
 });
